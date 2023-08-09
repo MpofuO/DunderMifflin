68597916c402a4f1f79499bf509b336b9249dcad
OMpofu <mpofuoliver1109@gmail.com>
2023-08-09 18:04:48 +0200

    Added_Description migration

--- Dunder Mifflin Paper Company/Migrations/20230809160302_Added_Description.Designer.cs
+++ Dunder Mifflin Paper Company/Migrations/20230809160302_Added_Description.Designer.cs
@@ -4,6 +4,7 @@ using Dunder_Mifflin_Paper_Company.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dunder_Mifflin_Paper_Company.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230809160302_Added_Description")]
+    partial class Added_Description
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Dunder Mifflin Paper Company/Migrations/20230809160302_Added_Description.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dunder_Mifflin_Paper_Company.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Description : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Product");
+        }
+    }
+}
