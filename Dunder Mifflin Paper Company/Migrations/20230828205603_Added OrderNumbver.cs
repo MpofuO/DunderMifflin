5e057d72703571a50a80b08edcb91e52388af8d0
OMpofu <mpofuoliver1109@gmail.com>
2023-08-28 22:59:15 +0200

    ...

---
+++ Dunder Mifflin Paper Company/Migrations/20230828205603_Added OrderNumbver.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dunder_Mifflin_Paper_Company.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedOrderNumbver : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
