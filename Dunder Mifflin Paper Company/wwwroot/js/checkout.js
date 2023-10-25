653a8cbdc4794ea9f4084bd605c4bbdcc96fef79
OMpofu <mpofuoliver1109@gmail.com>
2023-10-25 02:11:24 +0200

    Completed the checkout view

---
+++ Dunder Mifflin Paper Company/wwwroot/js/checkout.js
@@ -0,0 +1,40 @@
+function SelectDeliveryMethod(type) {
+    var form = document.forms.OrderReview;
+    form.DeliveryMethod.value = type;
+
+    let selected;
+    let unselected;
+
+    let addressDiv = document.getElementById('address');
+
+    if (type == 'Delivery') {
+        selected = document.getElementById('Delivery');
+        unselected = document.getElementById('Collection');
+        addressDiv.hidden = false;
+    }
+    else {
+        unselected = document.getElementById('Delivery');
+        selected = document.getElementById('Collection');
+        addressDiv.hidden = true;
+    }
+
+    selected.className = 'btn btn-primary';
+    unselected.className = 'btn btn-outline-primary';
+}
+
+function Show(show) {
+    moreAddresses = document.getElementById('moreAddresses');
+    btnMore = document.getElementById('showMore');
+    btnLess = document.getElementById('showLess');
+
+    if (show == 'more') {
+        btnMore.hidden = true;
+        moreAddresses.hidden = false;
+        btnLess.hidden = false;
+    }
+    else {
+        btnMore.hidden = false;
+        moreAddresses.hidden = true;
+        btnLess.hidden = true;
+    }
+}
