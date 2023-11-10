8203f497cb4f5a98b3f529787130749b051d7a27
OMpofu <mpofuoliver1109@gmail.com>
2023-11-10 06:21:34 +0200

    Added inventory management links on Product/List

--- Dunder Mifflin Paper Company/wwwroot/js/script.js
+++ Dunder Mifflin Paper Company/wwwroot/js/script.js
@@ -19,8 +19,30 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 4000);
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.overlay').forEach(a => {
+        a.addEventListener('click', function (e) {
+            e.preventDefault();
+        });
+    });
+});
+
 function GoBack() {
     window.history.go(-1);
 }
 
+var submit = function (formID) {
+    if (formID == 'Delete') {
+        if (confirm('Are you sure you want to delete this product from inventory?')) {
+            form.submit();
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        let form = document.getElementById(formID);
+        form.submit();
+    }
+}
 
